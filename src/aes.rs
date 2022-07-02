bbefd6364ff91c2ad9d360bb4640458102c4e418
amigin <amigin@gmail.com>
2022-07-02 10:36:17 +0300

    Made Descrypt with Result

--- src/aes.rs
+++ src/aes.rs
@@ -28,9 +28,14 @@ pub fn encrypt(aes_key: &AesKey, data: &[u8]) -> Vec<u8> {
     cipher.cbc_encrypt(&aes_key.iv, data)
 }
 
-pub fn decrypt(aes_key: &AesKey, data: &[u8]) -> Vec<u8> {
+pub fn decrypt(aes_key: &AesKey, data: &[u8]) -> Result<Vec<u8>, String> {
     let cipher = aes_key.get_cipher();
-    cipher.cbc_decrypt(&aes_key.iv, data)
+
+    let result = std::panic::catch_unwind(|| cipher.cbc_decrypt(&aes_key.iv, data));
+    match result {
+        Ok(result) => Ok(result),
+        Err(err) => Err(format!("AesKey: decryption failed: {:?}", err)),
+    }
 }
 
 #[cfg(test)]
@@ -49,8 +54,24 @@ mod test {
         let encrypted = super::encrypt(&key, plaintext);
 
         // Decryption
-        let decrypted = super::decrypt(&key, &encrypted[..]);
+        let decrypted = super::decrypt(&key, &encrypted[..]).unwrap();
 
         assert_eq!(decrypted, plaintext);
+    }
+
+    #[test]
+    pub fn encrypt_with_error() {
+        let my_key = b"This is the key!This is the key!This is 16 bytes";
+
+        let plaintext = b"My Phrase";
+        let key = AesKey::new(my_key);
+
+        // Encryption
+        let encrypted = super::encrypt(&key, plaintext);
+
+        // Decryption
+        let decrypted = super::decrypt(&key, &encrypted[..5]);
+
+        assert!(decrypted.is_err());
     }
 }
